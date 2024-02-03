c20c398c56e07178d1e51beaa7a6e5dfc3840ea3
明远湖之鱼 <488548816@qq.com>
2024-02-03 17:07:04 +0800

    feat: 组件的更新（二）

--- Vue3.0/packages/runtime-dom/src/modules/attrt.ts
+++ Vue3.0/packages/runtime-dom/src/modules/attrt.ts
@@ -3,6 +3,6 @@ export const patchAttr = (el, key, value) => {
   if (value === null) {
     el.removeAttribute(key);
   } else {
-    el.setAttribute(key);
+    el.setAttribute(key, value);
   }
 };
